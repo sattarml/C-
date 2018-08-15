8a21f82b2e2c46cc5687ad3366e5680a2f4031a8
sattar <42303993+sattarml@users.noreply.github.com>
2018-08-15 20:38:00 +0430

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //کدام عدد برزگتر است
+
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //if (a > b)
+            //    Console.WriteLine("a:" + a);
+            //else
+            //    Console.WriteLine("b: " + b);
+            //Console.ReadKey();
+
+
+
+            //ایا عدد ورودی فرد است یا ذوج
+            //Console.Write("Please enter a number : ");
+            //int n = int.Parse(Console.ReadLine());
+            //if (n % 2 == 1)
+            //    Console.WriteLine("Entered number is odd.");
+            //else
+            //    Console.WriteLine("Entered number is even.");
+            //Console.ReadKey(true);
+
+
+            //برسی اینکه ورودی بر 3 بخشبذیر هست یا ن
+            //Console.Write("pleas enter a nember:");
+            //int a = int.Parse(Console.ReadLine());
+            //if (a % 3 == 0)
+            //    Console.WriteLine("in adad bar 3 bakhshpazir ast");
+            //else
+            //    Console.WriteLine("in adad bar 3 bakhshpazir nist" );
+            //Console.ReadKey();
+
+
+            // خواندن نمره یک دانشجو از وردی و چاپabc
+            //Console.Write("please enter number:");
+            //int num = int.Parse(Console.ReadLine());
+            //if (num <= 10)
+            //    Console.WriteLine("g");
+            //else
+            //if (num <= 12)
+            //    Console.WriteLine("f");
+
+            //else
+            //if (num <= 14)
+            //    Console.WriteLine("c");
+            //else
+            //    if (num <= 16)
+            //    Console.WriteLine("b");
+            //else
+            //    if (num > 16)
+            //    Console.WriteLine("a");
+
+            //Console.ReadKey();
+
+
+
+
+            //قاعم الزاویه هست یا ن
+            //Console.Write("plese enter number:");
+            //double a = double.Parse(Console.ReadLine());
+            //double b = double.Parse(Console.ReadLine());
+            //double c = double.Parse(Console.ReadLine());
+            //if ( a ==  b + c)
+            //    Console.WriteLine("in mosalas ghaem ast");
+            //else
+            //    Console.WriteLine("nist");
+            //Console.ReadKey();
+
+
+
+
+            //حساب ضرایت معادله ی درجه ی 2
+            //Console.Write("Please Enter a : ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("Please Enter b : ");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.Write("Please Enter c : ");
+            //int c = int.Parse(Console.ReadLine());
+
+            //int delta = (b * b) - (4 * a * c);
+
+            //if (delta > 0)
+            //{
+            //    double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            //    double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+            //    Console.WriteLine("x1 = " + x1 + "\nx2 = " + x2);
+
+            //}
+            //else if (delta == 0)
+            //{
+
+            //    double x = (-b + Math.Sqrt(delta)) / (2 * a);
+            //    Console.WriteLine("x = " + x);
+
+            //}
+            //else
+            //{
+            //   Console.WriteLine("No Answer");
+            //}
+
+
+
+            //تبدیل درجه سانتی گراد به فارنهایت
+            //Console.Write("pls enter cantigerad: ");
+            //    double c = double.Parse(Console.ReadLine()), Result;
+            //Result = (c * 1.8) +32 ;
+            //Console.WriteLine( Result+"farenhite");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+        }
+    }
+}
